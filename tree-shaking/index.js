d5a631396266abdf27d22452edbfe8cbd3b88fc7
xuhaiqiang <xuhaiqiang@xiaoheiban.cn>
2021-09-01 15:09:24 +0800

    feat: 新增treeshaking

---
+++ tree-shaking/index.js
@@ -0,0 +1,59 @@
+// 实现 node index test.js
+const acorn = require('acorn');
+const l = console.log;
+const JSEmitter = require('./js-emitter');
+const fs = require('fs');
+// 获取命令行参数
+const args = process.argv[2];
+const buffer = fs.readFileSync(args).toString();
+const body = acorn.parse(buffer).body;
+const jsEmitter = new JSEmitter();
+let decls = new Map();
+let calledDecls = [];
+let code = [];
+// 遍历处理
+body.forEach(function (node) {
+  if (node.type == 'FunctionDeclaration') {
+    const code = jsEmitter.run([node]);
+    // l(code);
+    decls.set(jsEmitter.visitNode(node.id), code);
+    // l(decls); 'add' => 'function add(a,b){return a+b\n\n}\n'
+    return;
+  }
+  if (node.type == 'ExpressionStatement') {
+    if (node.expression.type == 'CallExpression') {
+      const callNode = node.expression;
+      calledDecls.push(jsEmitter.visitIdentifier(callNode.callee));
+      const args = callNode.arguments;
+      for (const arg of args) {
+        if (arg.type == 'Identifier') {
+          calledDecls.push(jsEmitter.visitNode(arg));
+        }
+      }
+    }
+  }
+  if (node.type == 'VariableDeclaration') {
+    const kind = node.kind;
+    for (const decl of node.declarations) {
+      decls.set(jsEmitter.visitNode(decl.id), jsEmitter.visitVariableDeclarator(decl, kind));
+    }
+    return;
+  }
+  if (node.type == 'Identifier') {
+    calledDecls.push(node.name);
+  }
+  code.push(jsEmitter.run([node]));
+});
+
+// l(calledDecls); // 被用到的函数或变量声明类型节点
+// l(decls); // 所有的函数或变量声明类型节点
+// l(code); // 其他所有没有被节点类型匹配的 AST 部分。
+
+// 生成 code
+code = calledDecls
+  .map((c) => {
+    return decls.get(c);
+  })
+  .concat([code])
+  .join('');
+fs.writeFileSync('test.shaked.js', code);
---
+++ tree-shaking/test.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+  return a + b;
+}
+
+function multiple(a, b) {
+  return a * b;
+}
+var firstOp = 9;
+
+var secondOp = 10;
+
+add(firstOp, secondOp);
+
+// treeShaking 摇掉 multiple
